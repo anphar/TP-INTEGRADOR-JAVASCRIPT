0e7f42f576e64f9b5989d2d6fe27d753cb99a242
Ana <a.aramayo.99@gmail.com>
2022-11-11 16:54:37 -0300

    JS RESUMEN COMPLETO

--- main.js
+++ main.js
@@ -1,53 +1,69 @@
-const cantidad = document.querySelector(".cantidad");
-const nombre = document.querySelector("#nombre");
-const apellido = document.querySelector("#apellido");
-const email = document.querySelector("#email");
+const precio = 200;
+let estudiante = 0.8;
+let trainee = 0.5;
+let junior= 0.15;
+
+let totalPagar;
+
+let cantidad = document.querySelector(".cantidad");
+let nombre = document.querySelector("#nombre");
+let apellido = document.querySelector("#apellido");
+let email = document.querySelector("#email");
 
 
-let precio = 200;
-const estudiante = 0.2;
-const trainee = 0.5;
-const junior= 0.85;
-
-const resumen = document.querySelector("#resumen");
+let resumen = document.querySelector("#resumen");
 resumen.addEventListener("click", descuento);
 
-const total = document.querySelector("#borrar");
-total.addEventListener("click", borrarTotal)
+let total = document.querySelector("#borrar");
+total.addEventListener("click", borrarTotal);
 
 function descuento(evento) {
     evento.preventDefault();
 
     if (Number(cantidad.value)) {
-        const categoria = document.querySelector(".categoria");
+        let categoria = document.querySelector(".categoria");
         
         switch (categoria.value) {
             case "0": {
-                precio *= cantidad.value;
+                totalPagar = precio * cantidad.value;
                 break;
             }
             case "estudiante": {
-                precio *= cantidad.value * estudiante;
+                totalPagar = precio * cantidad.value * estudiante;
                 break;
             }
             case "trainee": {
-                precio *= cantidad.value * trainee;
+                totalPagar = precio * cantidad.value * trainee;
                 break;
             }
             case "junior": {
-                precio *= cantidad.value * junior;
+                totalPagar = precio * cantidad.value * junior;
                 break;
             }
         }
 
-        document.querySelector("#total").innerHTML = `Total a pagar: $${precio}`;
+        document.querySelector(".total").innerHTML = `Total a pagar: $${totalPagar}`;
        
     } else {
         document.querySelector(".cantidad").style.border = "2px solid red";
         cantidad.value = "";
-        cantidad.placeholder = "Ingrese una cantidad";       
+        cantidad.placeholder = "Ingrese una cantidad";     
     }
+    
+}
 
+function borrarTotal() {
+
+    document.querySelector(".total").textContent = "Total a pagar:";
+
+    let form = document.querySelector(".ticketsForm");
+    for (i = 0; i < 4; i++) {
+        document.querySelector(".total" + form[i].className);
+    }
+}
+
+function validar () {
+    
     document.querySelector("#nombre").style.border = "2px solid red";
     nombre.value = "";
 
@@ -57,17 +73,8 @@ function descuento(evento) {
     document.querySelector("#email").style.border = "2px solid red";
     email.value = "";
 
+
     alert("Completar los campos en rojo");
 
-    borrarTotal(evento)
-}
-
-function borrarTotal() {
-
-    document.querySelector(".total").textContent = "Total a pagar:"
-
-    const form = document.querySelector(".ticketsForm");
-    for (i = 0; i < 4; i++) {
-        document.querySelector(".total" + form[i].className).style.border = "1px solid var(--gray-300)";
-    }
+    borrarTotal(evento);
 }
